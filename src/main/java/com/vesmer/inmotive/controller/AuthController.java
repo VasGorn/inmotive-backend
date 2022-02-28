03c9bafa6f750632e3b2cff59eb7cc51a4399f47
VasGorn <vas.goroshko@gmail.com>
2022-02-28 18:48:16 +0200

    feat: create the authentication rest controller

---
+++ src/main/java/com/vesmer/inmotive/controller/AuthController.java
@@ -0,0 +1,20 @@
+package com.vesmer.inmotive.controller;
+
+import com.vesmer.inmotive.service.AuthService;
+import com.vesmer.inmotive.service.RefreshTokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+    private final AuthService authService;
+    private final RefreshTokenService refreshTokenService;
+
+    @Autowired
+    public AuthController(AuthService authService, RefreshTokenService refreshTokenService) {
+        this.authService = authService;
+        this.refreshTokenService = refreshTokenService;
+    }
+}
