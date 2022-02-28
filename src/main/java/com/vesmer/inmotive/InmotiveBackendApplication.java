a6d41ff0c722140ccd6457c9690d875a8ca5bab0
VasGorn <vas.goroshko@gmail.com>
2022-02-28 10:55:33 +0200

    build: initialize the spring boot application

---
+++ src/main/java/com/vesmer/inmotive/InmotiveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vesmer.inmotive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmotiveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmotiveBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vesmer/inmotive/InmotiveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vesmer.inmotive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InmotiveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
