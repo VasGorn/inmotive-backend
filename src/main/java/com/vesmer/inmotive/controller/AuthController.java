57a8211abb5a187e7e4831b989b7927493857fb1
VasGorn <vas.goroshko@gmail.com>
2022-02-28 19:07:04 +0200

    feat: add endpoint for post request to sign up the user

--- src/main/java/com/vesmer/inmotive/controller/AuthController.java
+++ src/main/java/com/vesmer/inmotive/controller/AuthController.java
@@ -1,10 +1,16 @@
 package com.vesmer.inmotive.controller;
 
+import com.vesmer.inmotive.dto.RegisterRequest;
 import com.vesmer.inmotive.service.AuthService;
 import com.vesmer.inmotive.service.RefreshTokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -17,4 +23,11 @@ public class AuthController {
         this.authService = authService;
         this.refreshTokenService = refreshTokenService;
     }
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
+        authService.signup(registerRequest);
+        return new ResponseEntity<>("User Registration Successful", OK);
+    }
+
 }
