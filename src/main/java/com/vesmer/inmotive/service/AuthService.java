87bc09a84180f2a51e265013bd3530ddb3b8e455
VasGorn <vas.goroshko@gmail.com>
2022-03-01 10:56:28 +0200

    feat: add method to login user in authentication service class

--- src/main/java/com/vesmer/inmotive/service/AuthService.java
+++ src/main/java/com/vesmer/inmotive/service/AuthService.java
@@ -1,10 +1,17 @@
 package com.vesmer.inmotive.service;
 
+import com.vesmer.inmotive.dto.AuthenticationResponse;
+import com.vesmer.inmotive.dto.LoginRequest;
 import com.vesmer.inmotive.dto.RegisterRequest;
 import com.vesmer.inmotive.exception.InmotiveException;
 import com.vesmer.inmotive.model.User;
 import com.vesmer.inmotive.repository.UserRepository;
+import com.vesmer.inmotive.security.JwtProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,11 +23,21 @@ import java.time.Instant;
 public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtProvider jwtProvider;
+    private final RefreshTokenService refreshTokenService;
 
     @Autowired
-    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public AuthService(UserRepository userRepository,
+                       PasswordEncoder passwordEncoder,
+                       AuthenticationManager authenticationManager,
+                       JwtProvider jwtProvider,
+                       RefreshTokenService refreshTokenService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.jwtProvider = jwtProvider;
+        this.refreshTokenService = refreshTokenService;
     }
 
     public void signup(RegisterRequest registerRequest) {
@@ -45,5 +62,21 @@ public class AuthService {
         );
         user.setEnabled(true);
         userRepository.save(user);
+    }
+
+    public AuthenticationResponse login(LoginRequest loginRequest) {
+        Authentication authenticate =
+                authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                ));
+        SecurityContextHolder.getContext().setAuthentication(authenticate);
+        String token = jwtProvider.generateToken(authenticate);
+        return AuthenticationResponse.builder()
+                .authenticationToken(token)
+                .refreshToken(refreshTokenService.generateRefreshToken().getToken())
+                .expiresAt(Instant.now().plusMillis(jwtProvider.getJwtExpirationInMillis()))
+                .username(loginRequest.getUsername())
+                .build();
     }
 }
