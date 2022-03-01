e3484317918503870d90f09cd58005e4c1a20e65
VasGorn <vas.goroshko@gmail.com>
2022-03-01 12:06:30 +0200

    feat: add init method to load key store file

--- src/main/java/com/vesmer/inmotive/security/JwtProvider.java
+++ src/main/java/com/vesmer/inmotive/security/JwtProvider.java
@@ -1,13 +1,33 @@
 package com.vesmer.inmotive.security;
 
+import com.vesmer.inmotive.exception.InmotiveException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 
 @Service
 public class JwtProvider {
     private KeyStore keyStore;
     @Value("${jwt.expiration.time}")
     private Long jwtExpirationInMillis;
+
+    @PostConstruct
+    public void init() {
+        try {
+            keyStore = KeyStore.getInstance("JKS");
+            InputStream resourceAsStream = getClass().getResourceAsStream(
+                    "/inmotive.jks"
+            );
+            keyStore.load(resourceAsStream, "password".toCharArray());
+        } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e) {
+            throw new InmotiveException("Exception occurred while loading keystore", e);
+        }
+    }
 }
