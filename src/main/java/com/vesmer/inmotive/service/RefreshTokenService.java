52e328fb93173b7bb17a998eabac528c2b7ba4d0
VasGorn <vas.goroshko@gmail.com>
2022-03-02 08:31:51 +0200

    feat: add method to generate refresh token in service class

--- src/main/java/com/vesmer/inmotive/service/RefreshTokenService.java
+++ src/main/java/com/vesmer/inmotive/service/RefreshTokenService.java
@@ -1,10 +1,14 @@
 package com.vesmer.inmotive.service;
 
 
+import com.vesmer.inmotive.model.RefreshToken;
 import com.vesmer.inmotive.repository.RefreshTokenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.UUID;
 
 @Service
 @Transactional
@@ -14,5 +18,12 @@ public class RefreshTokenService {
     @Autowired
     public RefreshTokenService(RefreshTokenRepository refreshTokenRepository) {
         this.refreshTokenRepository = refreshTokenRepository;
+    }
+
+    public RefreshToken generateRefreshToken() {
+        RefreshToken refreshToken = new RefreshToken();
+        refreshToken.setToken(UUID.randomUUID().toString());
+        refreshToken.setCreated(Instant.now());
+        return refreshTokenRepository.save(refreshToken);
     }
 }
