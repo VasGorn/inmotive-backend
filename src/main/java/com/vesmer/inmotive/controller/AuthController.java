cbe79bc229119549e06bbd2bc65da0c6943be6f0
VasGorn <vas.goroshko@gmail.com>
2022-03-02 09:12:29 +0200

    feat: add endpoint for post request to refresh token

--- src/main/java/com/vesmer/inmotive/controller/AuthController.java
+++ src/main/java/com/vesmer/inmotive/controller/AuthController.java
@@ -2,12 +2,15 @@ package com.vesmer.inmotive.controller;
 
 import com.vesmer.inmotive.dto.AuthenticationResponse;
 import com.vesmer.inmotive.dto.LoginRequest;
+import com.vesmer.inmotive.dto.RefreshTokenRequest;
 import com.vesmer.inmotive.dto.RegisterRequest;
 import com.vesmer.inmotive.service.AuthService;
 import com.vesmer.inmotive.service.RefreshTokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 import static org.springframework.http.HttpStatus.OK;
 
@@ -38,5 +41,10 @@ public class AuthController {
     @PostMapping("/login")
     public AuthenticationResponse login(@RequestBody LoginRequest loginRequest) {
         return authService.login(loginRequest);
+    }
+
+    @PostMapping("/refresh/token")
+    public AuthenticationResponse refreshTokens(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+        return authService.refreshToken(refreshTokenRequest);
     }
 }
