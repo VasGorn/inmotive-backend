1a0dfece4777f408f4eec09917ff2ae33e86e00e
VasGorn <vas.goroshko@gmail.com>
2022-03-02 08:48:38 +0200

    feat: create refresh token repository interface that extends JpaRepository interface

---
+++ src/main/java/com/vesmer/inmotive/repository/RefreshTokenRepository.java
@@ -0,0 +1,9 @@
+package com.vesmer.inmotive.repository;
+
+import com.vesmer.inmotive.model.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+}
