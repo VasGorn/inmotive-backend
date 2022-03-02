f3f2378db0d222ed185f9d4b4e78a18e4405d611
VasGorn <vas.goroshko@gmail.com>
2022-03-02 14:16:43 +0200

    feat: add endpoint for post request to user logout

--- src/main/java/com/vesmer/inmotive/controller/AuthController.java
+++ src/main/java/com/vesmer/inmotive/controller/AuthController.java
@@ -7,6 +7,7 @@ import com.vesmer.inmotive.dto.RegisterRequest;
 import com.vesmer.inmotive.service.AuthService;
 import com.vesmer.inmotive.service.RefreshTokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -46,5 +47,12 @@ public class AuthController {
     @PostMapping("/refresh/token")
     public AuthenticationResponse refreshTokens(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
         return authService.refreshToken(refreshTokenRequest);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+        refreshTokenService.deleteRefreshToken(refreshTokenRequest.getRefreshToken());
+        return ResponseEntity.status(HttpStatus.OK).body("Refresh token deleted " +
+                "successfully!");
     }
 }
