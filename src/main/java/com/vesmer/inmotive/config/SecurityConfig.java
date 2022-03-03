375be798287982f52d9c0b9dbeaa82dbf7766a45
VasGorn <vas.goroshko@gmail.com>
2022-03-03 19:04:20 +0200

    feat: create security configuration class

---
+++ src/main/java/com/vesmer/inmotive/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.vesmer.inmotive.config;
+
+import com.vesmer.inmotive.security.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserDetailsService userDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Autowired
+    public SecurityConfig(UserDetailsService userDetailsService,
+                          JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.userDetailsService = userDetailsService;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+}
