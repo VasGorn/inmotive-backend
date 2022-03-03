7e691515344cdcc3cc8d69eb5babf59dc1e08878
VasGorn <vas.goroshko@gmail.com>
2022-03-03 19:34:07 +0200

    feat: add method to get password encoder in security configuration class

--- src/main/java/com/vesmer/inmotive/config/SecurityConfig.java
+++ src/main/java/com/vesmer/inmotive/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -25,5 +27,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
