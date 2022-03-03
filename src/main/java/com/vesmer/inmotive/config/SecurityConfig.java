9509652518f72b05279152bbddddf2a69ac25e0f
VasGorn <vas.goroshko@gmail.com>
2022-03-03 19:23:48 +0200

    feat: override method to get authentication manager

--- src/main/java/com/vesmer/inmotive/config/SecurityConfig.java
+++ src/main/java/com/vesmer/inmotive/config/SecurityConfig.java
@@ -2,6 +2,9 @@ package com.vesmer.inmotive.config;
 
 import com.vesmer.inmotive.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,5 +19,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                           JwtAuthenticationFilter jwtAuthenticationFilter) {
         this.userDetailsService = userDetailsService;
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
