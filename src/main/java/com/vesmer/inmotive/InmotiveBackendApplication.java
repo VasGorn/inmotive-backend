77cb3b65925a772546690bd9720b2fdc0b34981a
VasGorn <vas.goroshko@gmail.com>
2022-03-05 19:40:10 +0200

    feat: add annotation to import swagger configuration class

--- src/main/java/com/vesmer/inmotive/InmotiveBackendApplication.java
+++ src/main/java/com/vesmer/inmotive/InmotiveBackendApplication.java
@@ -1,9 +1,12 @@
 package com.vesmer.inmotive;
 
+import com.vesmer.inmotive.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SwaggerConfig.class)
 public class InmotiveBackendApplication {
 
 	public static void main(String[] args) {
