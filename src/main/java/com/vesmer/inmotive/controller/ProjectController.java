0850a5a258411ad17456a51d9ca2805d355713d5
VasGorn <vas.goroshko@gmail.com>
2022-03-12 20:25:15 +0200

    feat: add endpoint for post request to save project

--- src/main/java/com/vesmer/inmotive/controller/ProjectController.java
+++ src/main/java/com/vesmer/inmotive/controller/ProjectController.java
@@ -1,8 +1,12 @@
 package com.vesmer.inmotive.controller;
 
+import com.vesmer.inmotive.dto.ProjectDto;
 import com.vesmer.inmotive.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping("/api/v1/project")
@@ -12,5 +16,10 @@ public class ProjectController {
     @Autowired
     public ProjectController(ProjectService projectService) {
         this.projectService = projectService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProjectDto> saveProject(@RequestBody ProjectDto projectDto) {
+        return ResponseEntity.status(CREATED).body(projectService.save(projectDto));
     }
 }
