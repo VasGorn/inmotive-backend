5506875a1fb3a9299dfbf67c0357fba29225a966
VasGorn <vas.goroshko@gmail.com>
2022-03-12 19:08:47 +0200

    feat: create the project rest controller

---
+++ src/main/java/com/vesmer/inmotive/controller/ProjectController.java
@@ -0,0 +1,16 @@
+package com.vesmer.inmotive.controller;
+
+import com.vesmer.inmotive.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/project")
+public class ProjectController {
+    private final ProjectService projectService;
+
+    @Autowired
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+}
