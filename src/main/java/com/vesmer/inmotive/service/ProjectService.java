e05ae876db487458f9bde0464f5e6c7aa58ac58a
VasGorn <vas.goroshko@gmail.com>
2022-03-12 20:32:15 +0200

    feat: add method to save project in project service class

--- src/main/java/com/vesmer/inmotive/service/ProjectService.java
+++ src/main/java/com/vesmer/inmotive/service/ProjectService.java
@@ -1,10 +1,15 @@
 package com.vesmer.inmotive.service;
 
+import com.vesmer.inmotive.dto.ProjectDto;
 import com.vesmer.inmotive.mapper.ProjectMapper;
+import com.vesmer.inmotive.model.Project;
+import com.vesmer.inmotive.model.User;
 import com.vesmer.inmotive.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
 
 @Service
 @Transactional
@@ -19,5 +24,15 @@ public class ProjectService {
         this.projectRepository = projectRepository;
         this.authService = authService;
         this.projectMapper = projectMapper;
+    }
+
+    public ProjectDto save(ProjectDto projectDto) {
+        User user = authService.getCurrentUser();
+        projectDto.setCreated(Instant.now());
+        Project savedProject = projectRepository.save(
+                projectMapper.mapDtoToProject(projectDto, user)
+        );
+        projectDto.setId(savedProject.getId());
+        return projectDto;
     }
 }
