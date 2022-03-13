8828b1cc1b9cfc0bdb3b8acd142a7135a4f0160d
VasGorn <vas.goroshko@gmail.com>
2022-03-13 19:23:20 +0200

    feat: add method signature to get collection of projects by user in the repository interface

--- src/main/java/com/vesmer/inmotive/repository/ProjectRepository.java
+++ src/main/java/com/vesmer/inmotive/repository/ProjectRepository.java
@@ -1,9 +1,13 @@
 package com.vesmer.inmotive.repository;
 
 import com.vesmer.inmotive.model.Project;
+import com.vesmer.inmotive.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    Collection<Project> findByUser(User user);
 }
