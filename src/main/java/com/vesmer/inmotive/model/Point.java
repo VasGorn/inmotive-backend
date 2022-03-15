3837501cce50d4129ac61ce3a5b2508616e2597e
VasGorn <vas.goroshko@gmail.com>
2022-03-15 20:28:46 +0200

    feat: create the point model class with x and y axis value

---
+++ src/main/java/com/vesmer/inmotive/model/Point.java
@@ -0,0 +1,13 @@
+package com.vesmer.inmotive.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Point {
+    private double x;
+    private double y;
+}
