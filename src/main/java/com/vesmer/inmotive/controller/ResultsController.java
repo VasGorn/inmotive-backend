9309f158eb96bf225c1b10e7cae4749b7d3c10fb
VasGorn <vas.goroshko@gmail.com>
2022-03-17 20:27:22 +0200

    feat: add endpoint for get request to run calculation

--- src/main/java/com/vesmer/inmotive/controller/ResultsController.java
+++ src/main/java/com/vesmer/inmotive/controller/ResultsController.java
@@ -1,9 +1,16 @@
 package com.vesmer.inmotive.controller;
 
+import com.vesmer.inmotive.dto.ResultResponse;
+import com.vesmer.inmotive.model.CalculationStatus;
 import com.vesmer.inmotive.service.CalculationAsyncService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/api/v1/calculation")
@@ -13,5 +20,17 @@ public class ResultsController {
     @Autowired
     public ResultsController(CalculationAsyncService calcAsyncService) {
         this.calcAsyncService = calcAsyncService;
+    }
+
+    @GetMapping("/run/{projectId}")
+    public ResponseEntity<ResultResponse> runCalculation(@PathVariable Long projectId) {
+        boolean isContains = calcAsyncService.containsCalcEntry(projectId);
+        if (!isContains) {
+            calcAsyncService.asyncManager(projectId);
+        }
+        CalculationStatus status = calcAsyncService.getStatus(projectId);
+        ResultResponse response = new ResultResponse();
+        response.setStatus(status);
+        return ResponseEntity.status(OK).body(response);
     }
 }
