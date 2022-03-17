2ccb46a208b12209420b636817110b20a942ce34
VasGorn <vas.goroshko@gmail.com>
2022-03-17 20:34:54 +0200

    feat: add the concurrent map to collect calculation progress

--- src/main/java/com/vesmer/inmotive/service/CalculationAsyncService.java
+++ src/main/java/com/vesmer/inmotive/service/CalculationAsyncService.java
@@ -1,7 +1,13 @@
 package com.vesmer.inmotive.service;
 
+import com.vesmer.inmotive.model.CalculationStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Service
 public class CalculationAsyncService {
+    private final AtomicReference<ConcurrentHashMap<Long, CalculationStatus>>
+            calcProgressMap = new AtomicReference<>(new ConcurrentHashMap<>());
 }
