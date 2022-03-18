e73d5f6617b35795f1e92aef7981c03cccc7c9c0
VasGorn <vas.goroshko@gmail.com>
2022-03-18 20:22:35 +0200

    feat: add method to start the progress increment for simulate calculation

--- src/main/java/com/vesmer/inmotive/service/CalculationAsyncService.java
+++ src/main/java/com/vesmer/inmotive/service/CalculationAsyncService.java
@@ -1,8 +1,11 @@
 package com.vesmer.inmotive.service;
 
+import com.vesmer.inmotive.exception.InmotiveException;
 import com.vesmer.inmotive.model.CalculationStatus;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -10,4 +13,24 @@ import java.util.concurrent.atomic.AtomicReference;
 public class CalculationAsyncService {
     private final AtomicReference<ConcurrentHashMap<Long, CalculationStatus>>
             calcProgressMap = new AtomicReference<>(new ConcurrentHashMap<>());
+
+    @Async
+    public CompletableFuture<CalculationStatus> asyncManager(Long projectId) {
+        CalculationStatus calcStatus =
+                new CalculationStatus(true, 0, null);
+
+        calcProgressMap.get().put(projectId, calcStatus);
+        int progress = 0;
+        while (progress < 100) {
+            try {
+                Thread.sleep(123);
+            } catch (InterruptedException e) {
+                throw new InmotiveException("Error in calculation thread: " + projectId);
+            }
+            progress += 1;
+            calcStatus.setProgress(progress);
+        }
+        calcStatus.setRunning(false);
+        return CompletableFuture.completedFuture(calcStatus);
+    }
 }
