40b111238ec745a58fc8896461b44a45bb881a71
VasGorn <vas.goroshko@gmail.com>
2022-03-21 21:22:41 +0200

    feat: add endpoint for get request to check status of the calculation

--- src/main/java/com/vesmer/inmotive/controller/ResultsController.java
+++ src/main/java/com/vesmer/inmotive/controller/ResultsController.java
@@ -1,7 +1,9 @@
 package com.vesmer.inmotive.controller;
 
 import com.vesmer.inmotive.dto.ResultResponse;
+import com.vesmer.inmotive.exception.InmotiveException;
 import com.vesmer.inmotive.model.CalculationStatus;
+import com.vesmer.inmotive.model.ResultChartData;
 import com.vesmer.inmotive.service.CalculationAsyncService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -31,6 +33,27 @@ public class ResultsController {
         CalculationStatus status = calcAsyncService.getStatus(projectId);
         ResultResponse response = new ResultResponse();
         response.setStatus(status);
+        return ResponseEntity.status(OK).body(response);
+    }
+
+    @GetMapping("/status/{projectId}")
+    public ResponseEntity<ResultResponse> calculationStatus(@PathVariable Long projectId) {
+        boolean isContains = calcAsyncService.containsCalcEntry(projectId);
+        if (!isContains) {
+            throw new InmotiveException("Calculation not running in project" +
+                    " id: " + projectId);
+        }
+        CalculationStatus calcStatus = calcAsyncService.getStatus(projectId);
+        ResultChartData[] chartData = null;
+        if (!calcStatus.isRunning()) {
+            calcAsyncService.removeCalc(projectId);
+            chartData = inMemoryChartData();
+        }
+
+        ResultResponse response = new ResultResponse();
+        response.setStatus(calcStatus);
+        response.setChartsData(chartData);
+
         return ResponseEntity.status(OK).body(response);
     }
 }
